a6be0ae62c76f41be3943dce3a1704658e8ac8da
Jamie Painter <jamie.painter@rackspace.com>
2014-05-02 14:28:41 -0400

    PEP8, docstring
    Fixes docstrings and PEP8 compliance issues.

--- rax/fastcache/fastcache.py
+++ rax/fastcache/fastcache.py
@@ -12,128 +12,122 @@ A fast, just-in-time, in-memory cache for storing tokens/strings/objects which m
 Note 1: This module is not thread-safe
 Note 2: All times are expessed in seconds
 Note 3: Cache purging is only as fine-grained as the resolution. A cached value
-        may expire +/- the slice size. 
+        may expire +/- the slice size.
 """
 
-class FastCacheException(Exception): pass
+
+class FastCacheException(Exception):
+    pass
+
 
 class FastCache:
-  """
-  param: retention_period Retention period for cached object (in secs)
-  param: slice_size Size of a time slice (in secs)
-  """
-  def __init__(self, retention_period, slice_size):
-    if retention_period % slice_size != 0:
-      raise FastCacheException("Retention period must be evenly divisible by the slice_size")
 
-    self._slice_size = slice_size
-    self._slice_count = retention_period / self._slice_size
+    def __init__(self, retention_period, slice_size):
+        """Creates a new fastcache instance.
 
-    self._retention_period = retention_period
+        param: retention_period Retention period for cached object (in secs)
+        param: slice_size Size of a time slice (in secs)
+        """
+        if retention_period % slice_size != 0:
+            raise FastCacheException(
+                "Retention period must be evenly divisible by the slice_size")
 
-    self._current_time = int(time())
-    self._last_time = self._current_time # Time of the last cache insert
-    self._last_slice = self._timetoslice(self._last_time)
+        self._slice_size = slice_size
+        self._slice_count = retention_period / self._slice_size
 
-    self.clear()
+        self._retention_period = retention_period
 
-  """
-  Caches the specified value. 
+        self._current_time = int(time())
+        self._last_time = self._current_time  # Time of the last cache insert
+        self._last_slice = self._timetoslice(self._last_time)
 
-  Throws a FastCacheException if the value is already cached. 
-  """
-  def cache(self, value):
-    # This call will purge (which also sets self._current_time)
-    if self.is_cached(value):
-      raise FastCacheException("Value already cached")
+        self.clear()
 
-    self._time_hash[self._current_slice].append(value)
-    self._values[value] = True
+    def cache(self, value):
+        """Caches the specified value.
 
-  """
-  Determines if the specified value is cached. Returns True if it 
-  is found. Otherwise returns False. 
-  """
-  def is_cached(self, value):
-    self._purge()
-    return value in self._values
+        Throws a FastCacheException if the value is already cached.
+        """
+        # This call will purge (which also sets self._current_time)
+        if self.is_cached(value):
+            raise FastCacheException("Value already cached")
 
-  """
-  Clears all cached values
-  """
-  def clear(self):
-    # todo clear all 
-    self._values = {} # Holds the values
-    self._time_hash = [] # Holds the values indexed by time
-    
-    for x in xrange(0, self._slice_count):
-      self._time_hash.append([])
- 
-  """
-  Returns the total number of cached objects
-  """ 
-  def count(self):
-    self._purge()
-    return len(self._values)
+        self._time_hash[self._current_slice].append(value)
+        self._values[value] = True
 
-  """
-  Checks internal state of the cache and raises and exception
-  if any issue is detected. 
-  """
-  def integrity_check(self):
-    item_count = 0
+    def is_cached(self, value):
+        """Determines if the specified value is cached. Returns True if it
+        is found. Otherwise returns False.
+        """
+        self._purge()
+        return value in self._values
 
-    for slice_index in xrange(0, self._slice_count):
-      item_count += len(self._time_hash[slice_index])
+    def clear(self):
+        """ Clears all cached values """
+        # todo clear all
+        self._values = {}  # Holds the values
+        self._time_hash = []  # Holds the values indexed by time
 
-    if item_count != len(self._values):
-      raise FastCacheException("Integrity check failed")
+        for x in xrange(0, self._slice_count):
+            self._time_hash.append([])
 
-  """
-  (private) Purges all expired values from the cache
-  """
-  def _purge(self):
-    self._current_time = int(time())
-    self._current_slice = self._timetoslice(self._current_time)
+    def count(self):
+        """Returns the total number of cached objects """
+        self._purge()
+        return len(self._values)
 
-    time_delta = self._current_time - self._last_time
+    def integrity_check(self):
+        """Checks internal state of the cache and raises and exception
+        if any issue is detected.
+        """
+        item_count = 0
 
-    if time_delta > (self._retention_period - self._slice_size):
-      # We wrapped around (all cached values expired)
-      self.clear()
-    else:
+        for slice_index in xrange(0, self._slice_count):
+            item_count += len(self._time_hash[slice_index])
 
-      slice_delta = self._current_slice - self._last_slice
+        if item_count != len(self._values):
+            raise FastCacheException("Integrity check failed")
 
-      # Adjust the slice delta for the case where the the last slice's index
-      # is greater than the current slice's index.
-      if slice_delta < 0:
-        slice_delta = self._slice_count + slice_delta
+    def _purge(self):
+        """Purges all expired values from the cache"""
+        self._current_time = int(time())
+        self._current_slice = self._timetoslice(self._current_time)
 
-      if slice_delta > 0:
-        for x in xrange(self._last_slice+1, self._last_slice + slice_delta + 1):
-          s = x % self._slice_count
-          self._clear_slice(s)
-        
-    self._last_time = self._current_time
-    self._last_slice = self._current_slice
+        time_delta = self._current_time - self._last_time
 
-  """
-  (private) Clears a time slice with the specified index
-  param: slice_index The index of the slice to clear
-  """
-  def _clear_slice(self, slice_index):
-    delete_count = 0 # Sanity-check
+        if time_delta > (self._retention_period - self._slice_size):
+            # We wrapped around (all cached values expired)
+            self.clear()
+        else:
 
-    for item in self._time_hash[slice_index]:
-      del self._values[item]
-      delete_count += 1
+            slice_delta = self._current_slice - self._last_slice
 
-    assert(len(self._time_hash[slice_index]) == delete_count)
-    self._time_hash[slice_index] = []
+            # Adjust the slice delta for the case where the the last slice's index
+            # is greater than the current slice's index.
+            if slice_delta < 0:
+                slice_delta = self._slice_count + slice_delta
 
-  """
-  (private) Converts specified time to a slice index
-  """
-  def _timetoslice(self, t):
-    return (t / self._slice_size) % self._slice_count
+            if slice_delta > 0:
+                for x in xrange(self._last_slice + 1, self._last_slice + slice_delta + 1):
+                    s = x % self._slice_count
+                    self._clear_slice(s)
+
+        self._last_time = self._current_time
+        self._last_slice = self._current_slice
+
+    def _clear_slice(self, slice_index):
+        """Clears a time slice with the specified index
+        param: slice_index The index of the slice to clear
+        """
+        delete_count = 0  # Sanity-check
+
+        for item in self._time_hash[slice_index]:
+            del self._values[item]
+            delete_count += 1
+
+        assert(len(self._time_hash[slice_index]) == delete_count)
+        self._time_hash[slice_index] = []
+
+    def _timetoslice(self, t):
+        """Converts specified time to a slice index"""
+        return (t / self._slice_size) % self._slice_count
